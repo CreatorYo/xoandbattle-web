da0cd3e26ec0e2ddbc65244aaf4bf1d1159cc936
CreatorYo <unspeakableyo@gmail.com>
2025-12-04 21:57:07 +0000

    chore: remove console logs from service worker and ensure proper cache deletion

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,15 @@
 const CACHE_NAME = 'xobattle-v25';
 
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installing...');
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker activating...');
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
